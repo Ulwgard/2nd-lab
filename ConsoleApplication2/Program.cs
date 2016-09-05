3ed87de7b0785023a9a63e59ff8339831063b1e8
Vladislav Vladyslavych <flatronx19932@gmail.com>
2016-09-05 13:22:50 +0300

    first commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string age = "\tмоя строка 2";
+            string name = "\nмоя строка 1";
+            string weight = "\aмоя строка 3";
+
+            Console.Write(name);
+            Console.Write(age);
+            Console.Write(weight);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
